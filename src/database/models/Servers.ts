91322d73e79674a56f7a010f096bbc8f08ecc68a
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-27 11:35:36 +0100

    refactor(models): Server model changed to ts

---
+++ src/database/models/Servers.ts
@@ -0,0 +1,56 @@
+import { Document, model, Schema } from "mongoose"
+
+export interface IServer extends Document {
+    serverID: string,
+    name?: string,
+    bot?: {
+        prefix: string
+        color?: {
+            primary: string
+        }
+    },
+    channels?: {
+        sugerencias: string,
+        bienvenidas: string,
+        advertisments: string
+    },
+    roles?: {
+        warning: string,
+        admin: string,
+        invitado: string,
+        everyone: string
+    },
+    categories?: {
+        SOPORTE: String
+    }
+}
+
+const serverSchema = new Schema({
+    serverID: {
+        type: String,
+        required: true
+    },
+    name: String,
+    bot: {
+        prefix: String,
+        color: {
+            primary: String
+        }
+    },
+    channels: {
+        sugerencias: String,
+        bienvenidas: String,
+        advertisments: String
+    },
+    roles: {
+        warning: String,
+        admin: String,
+        invitado: String,
+        everyone: String
+    },
+    categories:{
+        SOPORTE: String
+    }
+})
+
+export default model<IServer>('Server', serverSchema)
